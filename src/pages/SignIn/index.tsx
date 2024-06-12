e90843ed8f3566ed545b27e62cd9bd06ce78d4ea
stenlyadam <stenly.adam@unklab.ac.id>
2024-06-12 14:40:08 +0800

    click event & event handler

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -8,15 +8,25 @@ const SignIn = () => {
   const emailSignIn = () => {
     console.log('Sign In With Email');
   };
+  const facebookSignIn = () => {
+    console.log('Sign In With Facebook');
+  };
+  const googleSignIn = () => {
+    console.log('Sign In With Google');
+  };
+
+  const appleSignIn = () => {
+    console.log('Sign In With Apple');
+  };
   return (
     <View style={styles.page}>
       <Title text="Welcome" />
       <TextInput label="Username" placeholder="Masukan username anda" />
       <TextInput label="Password" placeholder="Masukan password anda" />
       <Button text="Email Sign In" onPress={emailSignIn} />
-      <Button text="Facebook Sign In" color="blue" />
-      <Button text="Google Sign In" color="red" />
-      <Button text="Apple Sign In" color="black" />
+      <Button text="Facebook Sign In" color="blue" onPress={facebookSignIn} />
+      <Button text="Google Sign In" color="red" onPress={googleSignIn} />
+      <Button text="Apple Sign In" color="black" onPress={appleSignIn} />
     </View>
   );
 };
