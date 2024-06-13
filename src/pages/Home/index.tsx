1f653d1cf043dd9babc8f001819b7e1fc59f40f2
stenlyadam <stenly.adam@unklab.ac.id>
2024-06-13 14:57:11 +0800

    API integration using axios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,21 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Title from '../../components/Title';
 import Button from '../../components/Button';
 import TextInput from '../../components/TextInput';
+import axios from 'axios';
 
 const Home = () => {
   // let title = 'This is home page';
   const [title, setTitle] = useState('This is home page!!!');
   const [name, setName] = useState('');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data));
+  }, []);
 
   const handleSubmit = () => {
     // title = 'Welcome';
@@ -26,10 +34,18 @@ const Home = () => {
         onChangeText={handleInputNama}
       />
       <Button text="Click me" onPress={handleSubmit} />
+      {users.map(item => {
+        return <Text style={styles.users}>{item.name}</Text>;
+      })}
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  users: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+});
