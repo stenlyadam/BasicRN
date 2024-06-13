39d869406bbb25cdd3d6be8add4ebc51da609fd3
stenlyadam <stenly.adam@unklab.ac.id>
2024-06-13 14:38:33 +0800

    handle on change text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,19 +2,29 @@ import {StyleSheet, Text, View} from 'react-native';
 import React, {useState} from 'react';
 import Title from '../../components/Title';
 import Button from '../../components/Button';
+import TextInput from '../../components/TextInput';
 
 const Home = () => {
   // let title = 'This is home page';
   const [title, setTitle] = useState('This is home page!!!');
+  const [name, setName] = useState('');
 
   const handleSubmit = () => {
     // title = 'Welcome';
-    setTitle('Welcome');
+    setTitle(`Welcome ${name}`);
     // console.log(title);
+  };
+  const handleInputNama = e => {
+    setName(e);
   };
   return (
     <View>
       <Title text={title} />
+      <TextInput
+        label="Nama"
+        placeholder="Masukan nama anda"
+        onChangeText={handleInputNama}
+      />
       <Button text="Click me" onPress={handleSubmit} />
     </View>
   );
